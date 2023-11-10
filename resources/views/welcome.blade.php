48ae05c676e0af184c3dad28fb70a7cbe502a3ca
Manuel-kl <mannuehkipkirui@gmail.com>
2023-11-10 14:25:31 +0300

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="py-3 text-lg text-center mb-5">
-        <h3>Login or Register to view your manage your contacts</h3>
+        <h3>Login or Register to view and manage your contacts</h3>
     </div>
     <div class="flex flex-row items-center justify-center gap-5">
         <a href="/login" class="bg-blue-700  py-2 sm:py-3 px-4 sm:px-7 rounded-sm text-white hover:bg-blue-900 text-lg">Login</a>
